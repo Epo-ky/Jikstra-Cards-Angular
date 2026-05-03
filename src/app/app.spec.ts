ab2f42f9c7e168f084376ed84c4d06bd12ee04ce
Seu Nome <evando.oliveira@mv.com.br>
2026-05-03 16:46:36 -0300

    melhor

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,24 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+<<<<<<< HEAD
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+=======
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+>>>>>>> f6c6154836f6082cf0377f1f787c3a4c17b7a610
     }).compileComponents();
   });
 
   it('should create the app', () => {
+<<<<<<< HEAD
     const fixture = TestBed.createComponent(AppComponent);
+=======
+    const fixture = TestBed.createComponent(App);
+>>>>>>> f6c6154836f6082cf0377f1f787c3a4c17b7a610
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', async () => {
+<<<<<<< HEAD
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('JIKSTRA TCG');
+=======
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CardGameVisual');
+>>>>>>> f6c6154836f6082cf0377f1f787c3a4c17b7a610
   });
 });
--- src/app/card/card.spec.ts
+++ src/app/card/card.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<< HEAD
 import { CardComponent } from './card';
 
 describe('CardComponent', () => {
@@ -24,10 +25,30 @@ describe('CardComponent', () => {
       tipo: 'força'
     };
     fixture.detectChanges();
+=======
+import { Card } from './card';
+
+describe('Card', () => {
+  let component: Card;
+  let fixture: ComponentFixture<Card>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Card]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Card);
+    component = fixture.componentInstance;
+>>>>>>> f6c6154836f6082cf0377f1f787c3a4c17b7a610
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> f6c6154836f6082cf0377f1f787c3a4c17b7a610
